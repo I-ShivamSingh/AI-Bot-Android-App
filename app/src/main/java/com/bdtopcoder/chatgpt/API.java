330c9311b713d2edfcc544b2bff481cf93a53bc2
Shivam Singh <59681606+I-ShivamSingh@users.noreply.github.com>
2023-08-25 23:06:26 +0530

    Update API.java

--- app/src/main/java/com/bdtopcoder/chatgpt/API.java
+++ app/src/main/java/com/bdtopcoder/chatgpt/API.java
@@ -2,5 +2,5 @@ package com.bdtopcoder.chatgpt;
 
 public class API {
     public static String API_URL = "https://api.openai.com/v1/completions";
-    public static String API = "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";
+    public static String API = "Enter-Your-API-Here";
 }
