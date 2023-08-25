2602831e57a9b8b091455f7b8a82dc003a4ff342
Shivam Singh <shivamrocks.singh@gmail.com>
2023-08-25 22:57:34 +0530

    first commit

---
+++ app/src/main/java/com/bdtopcoder/chatgpt/API.java
@@ -0,0 +1,6 @@
+package com.bdtopcoder.chatgpt;
+
+public class API {
+    public static String API_URL = "https://api.openai.com/v1/completions";
+    public static String API = "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";
+}
